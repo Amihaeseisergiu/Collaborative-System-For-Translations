f97f7adf24eed7e2f667bab69ad86688c04e471c
Amihaeseisergiu <61450080+Amihaeseisergiu@users.noreply.github.com>
2021-10-25 00:17:29 +0300

    Added necessary files
    Spring boot, Thymeleaf, React, Tailwind

---
+++ src/main/java/com/amihaeseisergiu/csft/CollaborativeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amihaeseisergiu.csft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/amihaeseisergiu/csft/web/MainController.java
@@ -0,0 +1,19 @@
+package com.amihaeseisergiu.csft.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@AllArgsConstructor
+public class MainController {
+
+    @GetMapping
+    public String index()
+    {
+
+        return "index";
+    }
+}
