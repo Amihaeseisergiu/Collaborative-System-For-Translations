8e9433997c6f88a99332b39e0640e57a8c9f19f5
Amihaeseisergiu <61450080+Amihaeseisergiu@users.noreply.github.com>
2021-10-25 14:06:16 +0300

    File rearrangement

---
+++ mainapp/src/main/java/uaic/info/csft/mainapp/CollaborativeSystemApplication.java
@@ -0,0 +1,13 @@
+package uaic.info.csft.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeSystemApplication.class, args);
+	}
+
+}
---
+++ mainapp/src/main/resources/static/js/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="bg-blue-500">
+            <h1>React App</h1>
+        </div>
+    );
+}
